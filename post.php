27beed0055b5f447dadef92893277c032df7d1cc
andnune <andres_herrero@hotmail.es>
2015-08-14 12:53:10 +0200

    newIssue3

--- post.php
+++ post.php
@@ -7,26 +7,26 @@ if ((($_GET['whatever'] != ""))){
     $resultsComments = seleccComments($titulo);
 }
 ?>
-<? if (empty($results[0])): ?>
+<? if (empty($results)): ?>
     <h2>Error al seleccionar los datos</h2>
     <br>
 <? else: ?>
     <!--cargamos los resultados-->
-    <? foreach ($results as $post): ?>
+    <? //foreach ($results as $post): ?>
         <div class="panel ">
-            <div class="panel-heading"><b>Tittle: <h7><? echo $post['titulo'] ?></h7></b></div>
-            <div class="panel-body"><b>Author: <? echo $post['autor'] ?></b></div>
-            <div class="panel-body"><b>Body: <? echo $post['texto'] ?></b></div>
-            <div class="panel-body"><b>Date: <? echo $post['fecha'] ?></b></div>
+            <div class="panel-heading"><b>Tittle: <h7><? echo $results['titulo'] ?></h7></b></div>
+            <div class="panel-body"><b>Author: <? echo $results['autor'] ?></b></div>
+            <div class="panel-body"><b>Body: <? echo $results['texto'] ?></b></div>
+            <div class="panel-body"><b>Date: <? echo $results['fecha'] ?></b></div>
         </div>
-    <? endforeach; ?>
+    <?// endforeach; ?>
 <? endif; ?>
     <!-- cargamos el formulario de añadir comentarios-->
     <hr>
     <form id="formInsertComment" action='insertarComentario.php' method='post'>
         <h4>Nuevo comentario</h4>
         <input type='text' placeholder='Introduce Autor' size='100px' id='textoBorde' name='Autor'><br>
-        <input type='text' placeholder='Introduce titulo' size='100px' id='textoBorde' value="<? echo $post['titulo'] ?>"
+        <input type='text' placeholder='Introduce titulo' size='100px' id='textoBorde' value="<? echo $results['titulo'] ?>"
                name='Titulo' readonly><br>
         <input type='text' placeholder='Introduce Texto' size='100px' id='textoBorde' name='Texto'><br>
         <button class='btn btn-default' id='boton'><i class='glyphicon glyphicon-send'></i>Enviar</button>
