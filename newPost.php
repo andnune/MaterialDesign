ba0815162ebd603c38691622623e3380f4765e12
andnune <andres_herrero@hotmail.es>
2015-08-14 13:43:03 +0200

    endIssue2

--- newPost.php
+++ newPost.php
@@ -4,6 +4,6 @@
         <input type='text' placeholder="Fecha" size="100px" id='textoNoBorde' name='Fecha'><br>
         <input type='text' placeholder="Introduce titulo" size="100px" id='textoNoBorde' name='Titulo'><br>
         <input type='text' placeholder="Texto" size="100px" id='textoNoBorde' name='Texto'><br>
-        <button class="btn btn-default" id='botonNewPost'><i class="glyphicon glyphicon-eye-open"></i>Enviar
+        <button class="btn btn-success" id='botonNewPost'><i class="glyphicon glyphicon-eye-open"></i>Enviar
     </form>
 <? include ("footer.html"); ?>
