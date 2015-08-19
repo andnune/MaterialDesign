471e09b55416ce1f32a7b5cc678f5b75d267cd8d
andnune <andres_herrero@hotmail.es>
2015-08-19 15:05:59 +0200

    principioIssueMaterialDesign

--- index.php
+++ index.php
@@ -9,25 +9,25 @@ $cuenta=($results->getCount());
     <? $i=0; ?>
     <div class="row row1">
         <div class="col-lg-6 col-xs-12 col-sm-12 col-md-6">
-            <? for($i=0;$i<$cuenta;$i++):
-                $blog=$results->getItem($i);
+            <? //for($i=0;$i<$cuenta;$i++):
+                //$blog=$results->getItem($i);
                ?>
                 <div class="row">
                     <div class="col-lg-4">
-                        <img src="<? echo $blog->getAlgo('img') ?>" class="img-responsive"/>
+                        <img src="<?// echo $blog->getAlgo('img') ?>" class="img-responsive"/>
                     </div>
                     <div class="col-lg-8">
-                        <small class="text-muted"><? echo $blog->getAlgo('fecha') ?>
-                            por <? echo $blog->getAlgo('autor') ?></small>
-                        <h2><a href="post.php?whatever=<? echo $blog->getAlgo('id') ?>"><? echo $blog->getAlgo('titulo') ?></a>
+                        <small class="text-muted"><? //echo $blog->getAlgo('fecha') ?>
+                            por <? //echo $blog->getAlgo('autor') ?></small>
+                        <h2><a href="post.php?whatever=<? //echo $blog->getAlgo('id') ?>"><? //echo $blog->getAlgo('titulo') ?></a>
                         </h2>
-                        <? $textoCorto = $blog->getAlgo('texto');
-                        $textoFinal = substr($textoCorto, 0, 120); ?>
-                        <p><? echo $textoFinal ?></p>
+                        <? //$textoCorto = $blog->getAlgo('texto');
+                        //$textoFinal = substr($textoCorto, 0, 120); ?>
+                        <p><? //echo $textoFinal ?></p>
                     </div>
                 </div>
                 <hr>
-            <? endfor; ?>
+            <? //endfor; ?>
         </div>
 
 <? include 'footer.html'; ?>
